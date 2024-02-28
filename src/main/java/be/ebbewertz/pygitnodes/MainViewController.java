7ee160d350f4455000e7df895796f1476f2abd4b
Ebbe Wertz <ebbe.wertz8@gmail.com>
2024-02-28 23:10:43 +0100

    First working FXML view - controller class couple

--- src/main/java/be/ebbewertz/pygitnodes/MainViewController.java
+++ src/main/java/be/ebbewertz/pygitnodes/MainViewController.java
@@ -1,17 +1,29 @@
 package be.ebbewertz.pygitnodes;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController {
 
     @FXML
     public AnchorPane backgroundPane;
+    @FXML
+    public VBox pyNodesSelectionList;
+    @FXML
+    public Button createNewPyNodeButton;
 
     @FXML
     public void initialize(){
-        System.out.println("Hello from the controller");
-        backgroundPane.getChildren().add(new Label("hello"));
+        
+        createNewPyNodeButton.setOnAction(this::createNewPyNode);
+    }
+
+    private void createNewPyNode(ActionEvent actionEvent) {
+        Label label = new Label("PyNode");
+        pyNodesSelectionList.getChildren().add(label);
     }
 }
