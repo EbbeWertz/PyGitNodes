c87dc822b82de4155368cb7c98ddc3191fbb2775
Ebbe Wertz <ebbe.wertz8@gmail.com>
2024-02-28 23:39:01 +0100

    Working object to json translater

---
+++ src/main/java/be/ebbewertz/pygitnodes/Person.java
@@ -0,0 +1,34 @@
+package be.ebbewertz.pygitnodes;
+
+public class Person {
+    public String name;
+    public int age;
+
+    // Constructor, getters, and setters
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
+    }
+}
+
