a73fa9deb5212441f1031ca8b2b2d691aaf35cb1
Ebbe Wertz <ebbe.wertz8@gmail.com>
2024-02-28 23:38:45 +0100

    Working object to json translater

--- src/main/java/be/ebbewertz/pygitnodes/Main.java
+++ src/main/java/be/ebbewertz/pygitnodes/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 
@@ -15,6 +16,9 @@ public class Main extends Application {
         stage.setTitle("PyGitNodes");
         stage.setScene(scene);
         stage.show();
+        Person person = new Person("Ebbe", 19);
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(person));
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module be.ebbewertz.pygitnodes {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires com.google.gson;
 
     opens be.ebbewertz.pygitnodes to javafx.fxml;
     exports be.ebbewertz.pygitnodes;
